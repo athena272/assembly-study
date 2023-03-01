3dacbf1dfc4438c95a2d2205090863d3136a573c
athena272 <guilhermera272@gmail.com>
2023-03-01 18:57:26 -0300

    Create somar.asm

---
+++ Aula01/somar.asm
@@ -0,0 +1,29 @@
+.686
+.model flat, stdcall
+option casemap :none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\user32.inc
+include \masm32\include\msvcrt.inc
+
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\msvcrt.lib
+
+include \masm32\macros\macros.asm
+    
+.data
+    I DD 4
+    J DD 8
+    N DD 0
+
+.code
+start: 
+    mov eax,I
+    add eax,J
+    mov N,eax
+
+    printf("Valor de N: %d \n", N)
+    invoke ExitProcess, 0
+end start
